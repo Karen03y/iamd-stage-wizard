d43d21a46c7ebbff5714267ae0fc17e0acffb706
Oeyen Karen (s142053) <karen.oeyen@student.ap.be>
2024-03-21 10:18:46 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { OptionComponent } from "./components/option/option.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    standalone: true,
+    imports: [OptionComponent]
 })
 export class AppComponent {
   title = 'iamd-document-wizard';
