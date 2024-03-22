8f10707e23c70382a223801b3dda8a1ff72eb918
Oeyen Karen (s142053) <karen.oeyen@student.ap.be>
2024-03-22 10:22:38 +0100

    addColor function

--- src/app/components/option/option.component.ts
+++ src/app/components/option/option.component.ts
@@ -24,16 +24,25 @@ export class OptionComponent {
 
   ]
 
+/* TOGGLE CONTENT */
+  toggleContent(option:Option) {
+    option.showContent = !option.showContent;
+  }
+
+
+/* KLEUREN */
   colors = [
     { value: "#42A5F5" },
     { value: "#d4e157" },
     { value: "#ffb74d" }
   ].map((color, index) => ({ ...color, label: (index + 1).toString() }));
 
-  
-  toggleContent(option:Option) {
-    option.showContent = !option.showContent;
+  addColor()  {
+    const newIndex = this.colors.length+1;
+    const newColor={value:"#ffffff", label:newIndex.toString()};
+    this.colors.push(newColor)
   }
+
 
   
 }
