a0959bea6c69c3bc0232f960a0fbc90b52c6845f
Oeyen Karen (s142053) <karen.oeyen@student.ap.be>
2024-03-22 10:05:56 +0100

    fixed color picker animation

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { importProvidersFrom } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-bootstrapApplication(AppComponent, appConfig)
+
+bootstrapApplication(AppComponent, {providers:[importProvidersFrom(BrowserAnimationsModule)]})
   .catch((err) => console.error(err));
