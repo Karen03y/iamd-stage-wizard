5ebc7f9238cb9bc31a1e861cffd277ca2ef4ef19
Oeyen Karen (s142053) <karen.oeyen@student.ap.be>
2024-03-22 09:24:40 +0100

    first commits

--- src/app/components/option/option.component.ts
+++ src/app/components/option/option.component.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { NgxColorsModule } from 'ngx-colors';
+import { Option } from '../../../types';
+
 
 @Component({
   selector: 'app-option',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, NgxColorsModule, FormsModule],
   templateUrl: './option.component.html',
-  styleUrl: './option.component.css'
+  styleUrl: './option.component.css',
+  
 })
-export class OptionComponent {
+ 
 
+export class OptionComponent {
+  options:Option[] = [
+    {title:"Kleur", content : {type:"kleuren"}, showContent:true},
+    {title:"Header", content:{type:"header"}, showContent:false},
+    {title:"Body", content:{type:"body"}, showContent:false},
+    {title:"Footer", content:{type:"footer"}, showContent:false},
+
+  ]
+
+  color1:string="#42A5F5";
+  color2:string="#d4e157";
+  color3:string="#ffb74d";
+
+  toggleContent(option:Option) {
+    option.showContent = !option.showContent;
+  }
+
+  
 }
+
+
+
---
+++ src/types.ts
@@ -0,0 +1,10 @@
+export interface Option {
+    title: string;
+    content:Content,
+    showContent:boolean
+}
+
+export interface Content {
+    type:string,
+}
+
