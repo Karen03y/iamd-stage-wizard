0fc1c400fa441d50f0b24c2d3d3be2f66a62ed9e
Oeyen Karen (s142053) <karen.oeyen@student.ap.be>
2024-04-17 16:03:03 +0200

    footer component cleanup

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -8,7 +8,17 @@ import { LoadContentService } from '../../services/load-content.service';
   selector: 'app-footer',
   standalone: true,
   imports: [FormsModule, CommonModule],
-  templateUrl: './footer.component.html',
+  template: `
+    <h1>Kies een footer</h1>
+    <div class="footer" *ngFor="let footer of footers">
+      <div class="footer-content">
+        <div
+          class="footer-preview"
+          [innerHTML]="footer.content"
+          (click)="onFooterChange(footer)"
+        ></div>
+      </div>
+    </div>`,
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
@@ -16,17 +26,16 @@ export class FooterComponent {
 
   footers:Footer[] = [];
 
+  constructor(private loadContentService: LoadContentService) {}
 
-constructor(private loadContentService: LoadContentService) {}
+  ngOnInit() {
+    this.loadFooterContent();
+    console.log("Footer Component initiated")
+  }
 
-ngOnInit() {
-  this.loadFooterContent();
-}
-
-loadFooterContent() {
-  const footerFileNames = ['footer1.html', 'footer2.html']; 
-
-  footerFileNames.forEach(fileName => {
+  loadFooterContent() {
+    const footerFileNames = ['footer1.html', 'footer2.html']; 
+    footerFileNames.forEach(fileName => {
     this.loadContentService.loadContent(fileName, 'footer').subscribe((footer: Footer) => {
       this.footers.push(footer);
     }, error => {
@@ -35,8 +44,8 @@ loadFooterContent() {
   });
 }
 
-onFooterChange(footer:Footer) {
-  this.footerChange.emit(footer)
-}
+  onFooterChange(footer:Footer) {
+    this.footerChange.emit(footer)
+  }
 
 }
