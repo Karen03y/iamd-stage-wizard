ffaefcb0ad1c3c1b07a1d05a478df47f9524a6a3
Oeyen Karen (s142053) <karen.oeyen@student.ap.be>
2024-04-25 09:51:49 +0200

    reversed some changes

--- src/app/services/load-content.service.ts
+++ src/app/services/load-content.service.ts
@@ -1,73 +1,61 @@
 import { Injectable } from '@angular/core';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 import { HttpClient } from '@angular/common/http';
 import { Observable, forkJoin, throwError } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
 import { Header, Footer, Main } from '../../types';
-import { join } from 'path';
-import { saveAs } from 'file-saver';
-import { SafeHtml, DomSanitizer } from '@angular/platform-browser';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
 export class LoadContentService {
+
   private assetPaths = {
-    header: 'assets/headers/',
-    footer: 'assets/footers/',
-    main_VF: 'assets/mains/VF/',
-    main_AF: 'assets/mains/AF/',
-    main_Offertes: 'assets/mains/Offertes/',
+    'header': 'assets/headers/',
+    'footer': 'assets/footers/',
+    'main_VF': 'assets/mains/VF/',
+    'main_AF': 'assets/mains/AF/',
+    'main_Offertes': 'assets/mains/Offertes/'
   };
 
   constructor(private http: HttpClient, private sanitizer: DomSanitizer) {}
 
-  loadContent(
-    contentType: 'header' | 'footer' | 'main',
-    fileName: string
-  ): Observable<Header | Main | Footer> {
-    const baseAssetPath = 'assets/mains/';
-    const folderPath = contentType.slice(5);
-    const contentUrl = join(baseAssetPath, folderPath, fileName);
+  loadContent(fileName: string, contentType: 'header' | 'footer' | 'main_VF' | 'main_AF' | 'main_Offertes'): Observable<Header | Main | Footer> {
+    const contentUrl = `${this.assetPaths[contentType as keyof typeof this.assetPaths]}${fileName}`;
 
-    return this.http.get(contentUrl, { responseType: 'text' }).pipe(
-      map((contentData) => {
-        const sanitizedContent = this.sanitizer.bypassSecurityTrustHtml(
-          contentData
-        ) as SafeHtml;
+    return this.http.get(contentUrl, { responseType: 'text' })
+      .pipe(
+        map(contentData => {
+          const sanitizedContent = this.sanitizer.bypassSecurityTrustHtml(contentData) as SafeHtml;
 
-        switch (contentType) {
-          case 'header':
-          case 'footer':
-            return { content: sanitizedContent } as Header | Footer;
-          default:
-            return { content: sanitizedContent } as Main;
-        }
-      }),
-      catchError((error) => {
-        console.error('Failed to load content:', error);
-        return throwError(new Error('Failed to load content'));
-      })
-    );
+          switch (contentType) {
+            case 'header':
+            case 'footer':
+              return { content: sanitizedContent } as Header | Footer;
+            default:
+              return { content: sanitizedContent } as Main;
+          }
+        }),
+        catchError(error => {
+          console.error('Failed to load content:', error);
+          return throwError(new Error('Failed to load content'));
+        })
+      );
   }
 
-  // to do : filenames dynamisch inladen - via .json?
-
-  loadAllContent(
-    folderName: 'VF' | 'AF' | 'Offertes',
-    contentType: 'header' | 'footer' | 'main'
-  ): Observable<(Header | Main | Footer)[]> {
-    const mainFileNames = ['main1.html', 'main2.html'];
+  loadAllContent(folderName: 'VF' | 'AF' | 'Offertes', contentType: 'header' | 'footer' | 'main_VF' | 'main_AF' | 'main_Offertes'): Observable<(Header | Main | Footer)[]> {
+    const mainFileNames = ['main1.html', 'main2.html']; 
     const contentObservables: Observable<Header | Main | Footer>[] = [];
 
     mainFileNames.forEach((fileName) => {
-      const contentObservable = this.loadContent(contentType, fileName);
+      const contentObservable = this.loadContent(fileName, contentType);
       contentObservables.push(contentObservable);
     });
 
     return forkJoin(contentObservables).pipe(
-      catchError((error) => {
+      catchError(error => {
         console.error('Failed to load all content:', error);
-        return throwError(error);
+        return throwError(new Error('Failed to load all content'));
       })
     );
   }
